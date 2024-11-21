f0b3e0d618ec555c81c95a5bc9a02f780aabdcd6
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2024-11-21 22:10:47 +0100

    Finished lesson

--- 31_funkcja_print.py
+++ 31_funkcja_print.py
@@ -1,2 +1,19 @@
-print('test1')
-print('test2')
+print('FR')
+print('FR','USA','ESP','BE')
+
+france = 'FR'
+print(france, 'USA','ESP','BE')
+
+pi=3.14
+r=10
+print(pi, r)
+print(pi, r, pi*r*r) # wykonywanie obliczeń w funkcji print()
+print(pi, r, pi*r**2) # podnoszenie do potęgi, po dwóch gwiazdkach zapisujemy do jakiej potęgi
+
+print(1,2,3)
+print(1,2,3, sep='-') # dodawanie separatora
+print(france,'USA','ESP','BE', sep='\n') # separator w postaci nowej linii
+print(france,'USA','ESP','BE', sep='\t') # separator w postaci tabulatora
+
+print('Znak backshals: \\')
+print('\u03A3') # kod ASCII
