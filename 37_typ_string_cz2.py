7651e3f4dd98adf4959475429ed1dbcac1dd5a88
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2024-11-28 21:49:29 +0100

    End of lesson

---
+++ 37_typ_string_cz2.py
@@ -0,0 +1,11 @@
+drive = 'c:\\'
+folder = 'scripts\\python\\'
+file = 'myscript.py'
+path = drive + folder + file # konkatenacja
+print(path)
+
+new_text1 = 'text with\nnew line' # znak nowej linii w tekście
+print(new_text1)
+
+new_text2 = r'text with\nnew line' # surowy tekst, znaki specjalne są ignorowane
+print(new_text2)
