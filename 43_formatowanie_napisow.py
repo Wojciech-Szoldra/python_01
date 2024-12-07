83ef10ff493dcaafb941733adf01101ce5baee39
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2024-12-07 20:44:25 +0100

    End of practical part

--- 43_formatowanie_napisow.py
+++ 43_formatowanie_napisow.py
@@ -19,3 +19,26 @@ print(message6.format(100,'File1.txt'))
 
 message7 = 'File {0:20s} has size {1:10d} KB' # Stała ilość znaków
 print(message7.format('file1.txt',100))
+
+# *** Formatowanie napisów - LAB ***
+
+hello_message = 'Hello %s!'
+print(hello_message % ('Kate'))
+print(hello_message % ('Chris'))
+
+hello_message2 = 'Hello {0:s}!'
+print(hello_message2.format('Kate'))
+print(hello_message2.format('Chris'))
+
+hello_message3 = '%s has %d %s'
+print(hello_message3 % ('Kate',1,'animal'))
+print(hello_message3 % ('Chris',100000,'$'))
+
+hello_message4 = "{0:s} has {1:d} {2:s}"
+print(hello_message4.format('Kate',1,'animal'))
+print(hello_message4.format('Chris',100000,'$'))
+
+line = '{0:20s} costs {1:10d} €'
+print(line.format('BOOK',10))
+print(line.format('ICE CREAM',3))
+print(line.format('TRIP TO VENICE',119))
