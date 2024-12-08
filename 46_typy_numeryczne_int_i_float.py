3f1f4f5ab6890c7c20166eab18845dd9aca04176
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2024-12-08 16:03:34 +0100

    End of practical part

--- 46_typy_numeryczne_int_i_float.py
+++ 46_typy_numeryczne_int_i_float.py
@@ -23,3 +23,14 @@ print(five//three) # dzielenie całkowite
 print(five % three) # dzielenie modulo
 
 print(float('inf') > 9999999999999999999) # string inf rzutowany na float jest interpretowany jako nieskończoność
+
+# *** Formatowanie napisów i typy numeryczne - LAB ***
+
+name = 'John'
+age = 35
+days_in_year = 365
+days_old = age * days_in_year
+message = '{0:s} is {1:d} years old, sa is about {2:d} days old'
+print(message.format(name,age,days_old))
+
+print(f'1234567890 devided by 12345 is {1234567890//12345} and the rest is {1234567890 % 12345}')
