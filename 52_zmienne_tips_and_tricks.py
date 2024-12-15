4afc1378b8aa4fb272c79e5e036d408a4932ab17
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2024-12-15 19:42:56 +0100

    End of the practical part

--- 52_zmienne_tips_and_tricks.py
+++ 52_zmienne_tips_and_tricks.py
@@ -36,3 +36,18 @@ print('x:',x)
 
 x -= 1
 print('x:',x)
+
+# *** Zmienne Tips & Tricks. Kolejność działań - LAB ***
+
+v1 = 126
+v2 = '126'
+v3 = 126.0
+v4 = '126.0'
+
+print(type(v1),type(v2),type(v3),type(v4))
+print(type(v1+v3))
+print(type(v2+v4))
+print(7-1*0+3+3)
+print((4**5)/(2**3))
+
+print(99+9/9)
