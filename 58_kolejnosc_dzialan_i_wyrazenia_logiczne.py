c06f11c89da9947bc1913ee86b1722fe2f518402
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2024-12-16 22:11:20 +0100

    End of the practical part

---
+++ 58_kolejnosc_dzialan_i_wyrazenia_logiczne.py
@@ -0,0 +1,14 @@
+# *** LAB ***
+
+print((45*5+50)*20+1024-1989)
+print((8*2+10)/2-8)
+
+presence = 40
+average = 2.5
+term_paper = True
+
+passing_1 = (presence >= 80 and average >= 3 ) or term_paper
+print(passing_1)
+
+passing_2 = presence >= 80 and average >=3 and term_paper
+print(passing_2)
