3a5b50562f736b51cffd2dd842610abb883268b4
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2024-12-21 21:26:40 +0100

    End of the practical part

--- 60_listy.py
+++ 60_listy.py
@@ -32,3 +32,50 @@ print(countries)
 countries_copy = countries # dwie zmienne wskazują na tą samą listę
 countries_copy.clear() # usunięcie wszystkich elementów z listy
 print(countries_copy, countries)
+
+countries = ['FR', 'USA', 'DE', 'RU']
+countries_copy = countries.copy() # skopiowanie i utworzenie nowej listy, każda zmienna to osobna lista
+
+# *** Listy - LAB ***
+
+hits_titles = ['BROTHERS IN ARMS','BOHEMIAN RHAPSODY','STAIRWAY TO HEAVEN','RIDERS ON THE STORM','WISH YOU WERE HERE']
+print(hits_titles)
+
+hits_titles.append('CHILD IN TIME')
+hits_titles.append('AGAIN')
+print(hits_titles)
+
+hits_titles.insert(2,'HOTEL CALIFORNIA')
+print(hits_titles)
+
+hits_titles.insert(0,'THE SOUND OF SILENCE')
+print(hits_titles)
+
+print(hits_titles.index('THE SOUND OF SILENCE'))
+
+hits_titles.remove('HOTEL CALIFORNIA')
+print(hits_titles)
+
+hits_titles[0] = 'ENJOY THE SILENCE'
+print(hits_titles)
+
+hits_to_read = hits_titles.copy()
+hits_to_read.reverse()
+print(hits_to_read)
+
+hits_to_read.sort()
+print(hits_to_read)
+
+print(hits_to_read.pop(0))
+print(hits_to_read.pop(1))
+print(hits_to_read)
+
+aditional_songs = ['NOTHING COMPARES 2 U','WISH YOU WERE HERE']
+hits_to_read.extend(aditional_songs)
+print(hits_to_read)
+
+print(hits_to_read.count('RIDERS ON THE STORM'))
+print(hits_to_read.count('WISH YOU WERE HERE'))
+
+hits_to_read.clear()
+print(hits_to_read)
