4dbe976b6e80be6c2385054d4f6e92d5b833f019
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-01-15 22:03:43 +0100

    End of the practical part

--- 69_instrukcja_warunkowa_if.py
+++ 69_instrukcja_warunkowa_if.py
@@ -41,3 +41,31 @@ if not is_drunk:
     print('Jeżeli is_drunk będzie False to warunek się spełni ponieważ zostanie odwrócony = True')
 else:
     print('Jeżeli is_drunk będzie True to warunek się nie spełni ponieważ zostanie odwrócony = False')
+
+# *** Instrukcja warunkowa IF - LAB ***
+
+likes = 500
+shares = 100
+
+if likes >= 500 and shares >= 100:
+    print('PROMOTION -10%')
+else:
+    print("We haven't reached our goal yet")
+
+pizza = True
+large_soda = False
+is_weekend = True
+
+if is_weekend and (pizza or large_soda):
+    print('You get a coupon for a free hamburger')
+else:
+    print('Buy a pizza or large drink during the week to receive a coupon.')
+
+disk_size = 140
+disk_size_used = 133
+file_size = 1
+
+if disk_size >= (disk_size_used + file_size):
+    print('Enough space')
+else:
+    print('Not enough space')
