198ff3c9b2f31c73d9eaf4bfd4a4cc663a3b7c1c
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-01-15 20:13:19 +0100

    Tests

---
+++ 69_instrukcja_warunkowa_if.py
@@ -0,0 +1,6 @@
+isDrunk = False
+
+if not isDrunk:
+    print('Jeżeli isDrunk będzie False to warunek się spełni ponieważ zostanie odwrócony = True')
+else:
+    print('Jeżeli isDrunk będzie True to warunek się nie spełni ponieważ zostanie odwrócony = False')
