3c5ddad0b832aec1abfa5aa372d95811ec017f26
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-01-19 15:41:07 +0100

    End of the practical part

--- 75_ternary_operator.py
+++ 75_ternary_operator.py
@@ -26,3 +26,51 @@ else:
 # Operator trójargumentowy (ternary operator)
 
 print('we stay at home' if it_rains else 'we go out')
+
+# *** IF - LAB ***
+
+print('LAB\n\nzad_1\n')
+
+muscle_pain = False
+fever = False
+weaknes = False
+is_man = False
+
+print('1')
+
+if muscle_pain and fever and weaknes:
+    print('Suspicion of influenza')
+else:
+    print('The flu is unlikely')
+
+print('Suspicion of influenza' if muscle_pain and fever and weaknes else 'The flu is unlikely')
+
+print('2')
+
+if muscle_pain and fever and weaknes:
+    print('Suspicion of influenza')
+elif weaknes and (not fever or not muscle_pain):
+    print('Just take a rest!')
+elif muscle_pain or fever:
+    print('You may be cold')
+else:
+    print('The flu is unlikely')
+
+print('3')
+
+if muscle_pain and fever and weaknes:
+    print('Suspicion of influenza')
+elif is_man and (muscle_pain or fever or weaknes):
+    print('Suspicion of influenza')
+elif weaknes and (not fever or not muscle_pain):
+    print('Just take a rest!')
+elif muscle_pain or fever:
+    print('You may be cold')
+else:
+    print('The flu is unlikely')
+
+print('4')
+
+is_check_completed = False
+
+print('CHECK IS COMPLETED' if is_check_completed else 'CHECK NOT DONE YET!')
