efcea3b24cc039fe83d5a308ab2188bb508468e0
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-01-23 21:31:26 +0100

    End of session 2

--- 78_petla_while_else.py
+++ 78_petla_while_else.py
@@ -8,3 +8,13 @@ while licznik <= 5:
     licznik += 1
 else:
     print('Pętla zakończyła się normalnie')
+
+# Funkcja len()
+
+numbers = [1,2,3,4,5]
+elements1 = len(numbers)
+print(f'Numbers have {elements1} elements')
+
+letters = 'example'
+elements2 = len(letters)
+print(f'Letters have {elements2} elements')
