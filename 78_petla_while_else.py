d1dafe763e511547ad65e62dfa26976ddd13911d
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-01-25 19:57:19 +0100

    End of the lesson

--- 78_petla_while_else.py
+++ 78_petla_while_else.py
@@ -1,3 +1,16 @@
+# Przykład z lekcji
+
+i = 10
+i_min = 0
+
+while i >= i_min:
+    print(i, 'I like Python')
+    i -= 1
+else:
+    print("Now i =",i)
+
+# Poniższa pętla zostanie przerwana przez instrukcję break - instrukcja else nie zostanie wykonana
+
 licznik = 1
 
 while licznik <= 5:
@@ -8,6 +21,21 @@ while licznik <= 5:
     licznik += 1
 else:
     print('Pętla zakończyła się normalnie')
+
+# Poniższa pętla nie zostanie przerwana przez instrukcję break - wykona się instrukcja else
+
+list1 = [1,2,3,5,7,9]
+searched = 4
+i = 0
+
+while i < len(list1):
+    print(list1[i])
+    if list1[i] == searched:
+        print(f'Number found: {searched}')
+        break
+    i += 1
+else:
+    print(f'Number {searched} not found')
 
 # Funkcja len()
 
