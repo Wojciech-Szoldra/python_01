978a84e2bed5057d918947637e83a030ccf33bba
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-01-26 16:20:44 +0100

    End of session 1

--- 81_if_w_while.py
+++ 81_if_w_while.py
@@ -1,3 +1,5 @@
+# Poniższa pętla wyszukuje w liście values wszystkie ciągi trzech liczb, ustawionych rosnąco
+
 values = [10,43,12,48,12,11,18,98,57,28,19,27,49,19,74]
 
 i = 0
@@ -9,4 +11,22 @@ while i < max:
     if values[i] < values[i+1] and values[i+1] < values[i+2]:
         print('\tFound: ',values[i],values[i+1],values[i+2])
 
+    i+=1
+
+# *** If w while - LAB ***
+
+print('\n*** LAB ***')
+print('\nTask 1\n')
+
+numbers = [8, 18, 2, 4, 16, 5, 25, 4, 22, 3, 3, 5, 3, 9, 81, 11]
+
+i = 0
+max = len(numbers) -1
+
+while i < max:
+    print(f'{i}:',numbers[i],numbers[i+1])
+
+    if numbers[i+1] == numbers[i]**2:
+        print(f'\tFound: {numbers[i],numbers[i+1]}')
+    
     i+=1
