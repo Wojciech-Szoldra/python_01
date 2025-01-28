7a77f80da1b0016cd09823d6b3354ba8303ee255
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-01-28 23:06:54 +0100

    End of session 2

--- 84_petla_while_ladowanie_paczek.py
+++ 84_petla_while_ladowanie_paczek.py
@@ -1,4 +1,5 @@
-#
+# Pętla umieszcza w liście box elementy z listy cargo w taki sposób,
+# aby być jak najbliżej maksymalnej wartości ustalonej w box_capacity
 
 cargo = [40,20,4,5,30,8,2,7,3,19,32,40,20,35,15,32,9]
 cargo.sort()
@@ -17,3 +18,15 @@ while i < len(cargo) and (box_capacity - sum(box)) >= min(cargo):
 
 print('The collected items sum is:', sum(box))
 print('The elements are:', box)
+
+# *** If w while - LAB ***
+
+print('\n*** LAB ***')
+print('\nTask 1\n')
+
+num_max = 50
+i = 0
+
+while i < 50:
+    print(f'{i} + {i+1} = {i+(i+1)}')
+    i+=1
