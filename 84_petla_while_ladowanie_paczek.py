ea91fc0ffb83243c3e123acedc64da11368a2206
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-01-29 20:52:07 +0100

    End of session 3

--- 84_petla_while_ladowanie_paczek.py
+++ 84_petla_while_ladowanie_paczek.py
@@ -30,3 +30,29 @@ i = 0
 while i < 50:
     print(f'{i} + {i+1} = {i+(i+1)}')
     i+=1
+
+print('\nTask 2\n')
+
+import random
+
+my_number = random.randint(0,20) # generuje losową liczbę z przedziało 0-20
+guess = -1 # przypisujemy wartość, która na pewno różni się od tej z przedziału 0 - 20
+trials = 0
+
+print('Guessing game\n')
+
+while guess != my_number:
+    guess = int(input('Guess my number: '))
+    
+    trials+=1
+
+    if guess == my_number:
+        print(f'Congratulations! You guessed it on the {trials} try!')
+    elif guess > my_number:
+        print('Sorry- my number is smaller than your guess, Try again!')
+    else:
+        print('Sorry- my number is greater than your guess, Try again!')
+
+    
+
+   
