b823b2c1e3fad6c672be4054249763e0bc6379b3
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-02-03 16:27:00 +0100

    End of session 1

---
+++ 90_petla_for.py
@@ -0,0 +1,17 @@
+# Prosta pętla for
+owoce = ['jabłko', 'banan', 'wiśnia']
+
+for owoc in owoce:
+    print(owoc)
+
+print()
+
+persons = ['Elizabeth','Steven','Sebastian','Margaret','Svetlana','Raphael']
+
+domain = 'mycompany.com'
+
+for person in persons:
+    email = person + '@' + domain
+    print('Email for\t', person, '\tis\t', email)
+else:
+    print('-- end of the list --')
