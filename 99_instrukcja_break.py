9bb336e5d367d98a40eae211f436441306283b3e
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-02-17 23:29:19 +0100

    Session 1.2

--- 99_instrukcja_break.py
+++ 99_instrukcja_break.py
@@ -16,3 +16,11 @@ for num in numbers:
         print(f'Liczba {num} to nie poszukiwana liczba') 
 
 print('Koniec programu')
+
+print('\n*** LAB ***')
+print('\nTask 1\n')
+
+text = 'Mechanical advantage is a measure of the force amplification achieved by using a tool, mechanical device or machine system. The device preserves the input power and simply trades off forces against movement to obtain a desired amplification in the output force. The model for this is the law of the lever. Machine components designed to manage forces and movement in this way are called mechanisms.[1] An ideal mechanism transmits power without adding to or subtracting from it. This means the ideal mechanism does not include a power source, is frictionless, and is constructed from rigid bodies that do not deflect or wear. The performance of a real system relative to this ideal.'
+
+words = text.split(' ')
+print(words)
