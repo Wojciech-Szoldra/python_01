1afca2b030f003bb837ad8240deb1e98fd8bbc5d
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-02-20 23:29:57 +0100

    Session 1.2

--- 102_instrukcja_continue.py
+++ 102_instrukcja_continue.py
@@ -17,7 +17,7 @@ emails = []
 
 for person in persons:
 
-    if '@' in person:
+    if '@' in person: # Ciekawe, że w ten sposób można sprawdzać znaki
         emails.append(person)
         continue
 
@@ -26,3 +26,66 @@ for person in persons:
 
 for email in emails:
     print(email)
+
+print('\n*** LAB ***')
+print('\nTask 1\n')
+
+menu = '''
+Choose what you want me to do for you:
+1 - COFFEE
+2 - TEA
+3 - MAKE ME SMILE
+---------------
+To stop this script select 0
+'''
+
+smile = '''
+                          oooo$$$$$$$$$$$$oooo
+                      oo$$$$$$$$$$$$$$$$$$$$$$$$o
+                   oo$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$o         o$   $$ o$
+   o $ oo        o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$o       $$ $$ $$o$
+oo $ $ "$      o$$$$$$$$$    $$$$$$$$$$$$$    $$$$$$$$$o       $$$o$$o$
+"$$$$$$o$     o$$$$$$$$$      $$$$$$$$$$$      $$$$$$$$$$o    $$$$$$$$
+  $$$$$$$    $$$$$$$$$$$      $$$$$$$$$$$      $$$$$$$$$$$$$$$$$$$$$$$
+  $$$$$$$$$$$$$$$$$$$$$$$    $$$$$$$$$$$$$    $$$$$$$$$$$$$$  """$$$
+   "$$$""""$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     "$$$
+    $$$   o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     "$$$o
+   o$$"   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$       $$$o
+   $$$    $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" "$$$$$$ooooo$$$$o
+  o$$$oooo$$$$$  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   o$$$$$$$$$$$$$$$$$
+  $$$$$$$$"$$$$   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     $$$$""""""""
+ """"       $$$$    "$$$$$$$$$$$$$$$$$$$$$$$$$$$$"      o$$$
+            "$$$o     """$$$$$$$$$$$$$$$$$$"$$"         $$$
+              $$$o          "$$""$$$$$$""""           o$$$
+               $$$$o                                o$$$"
+                "$$$$o      o$$$$$$o"$$$$o        o$$$$
+                  "$$$$$oo     ""$$$$o$$$$$o   o$$$$""
+                     ""$$$$$oooo  "$$$o$$$$$$$$$"""
+                        ""$$$$$$$oo $$$$$$$$$$
+                                """"$$$$$$$$$$$
+                                    $$$$$$$$$$$$
+                                     $$$$$$$$$$"
+                                      "$$$""""
+'''
+
+while True: # pętla wykonywała się będzie w nieskończoność
+    print(menu)
+    letter = input('Choose between 1 and 3: ' )
+
+    if letter == '1':
+        print('Function COFFEE not implemented!')
+        input('Press enter')
+        continue
+
+    if letter == '2':
+        print('Function TEA not implemented!')
+        input('Press enter')
+        continue
+
+    if letter == '3':
+        print(smile)
+        input('Press enter')
+        continue
+
+    if letter == '0':
+        break # przerywa działanie pętli czyli wychodzimy z nieskończonej pętli
