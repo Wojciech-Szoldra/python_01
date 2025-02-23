65d36e85604a39b1cef279b021ee8b18bbd3b828
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-02-23 18:32:11 +0100

    Session 2.1

--- 105_while_lab.py
+++ 105_while_lab.py
@@ -8,10 +8,10 @@ total_profit = initial_capital
 year = 0
 
 while year < max_time_years:
-   yeraly_profit = total_profit * percent/100
-   total_profit += yeraly_profit
-   year += 1
-   print(f'Year {year} summary: {round(total_profit, 2)}')
+      yeraly_profit = total_profit * percent/100
+      total_profit += yeraly_profit
+      year += 1
+      print(f'Year {year} summary: {round(total_profit, 2)}')
 
 else:
       print(f'Earnings amount: {round((total_profit - initial_capital),2)}')
@@ -19,17 +19,30 @@ else:
 print('\nTask 2\n')
 
 number = 20730906
+sum = 0
 
-number_str = str(number) # konwersja liczby na ciąg znaków
-
-print(number_str)
-
-digits = [int(digit) for digit in number_str] # list comprehension / iterować możemy po stringach
-
-print(digits)
+print('My approach\n')
 
 sum = 0
 
-for digit in digits:
-    sum += digit
-    print(sum)
+while sum <= 0: # Pętla wykona się tylko raz
+      number_str = str(number) # konwersja liczby na ciąg znaków
+      digits = [int(digit) for digit in number_str] # utworzenie tablicy z cyframi
+
+      for digit in digits:
+            sum += digit # Pętla iteruje po cyfrach, a my je sumujemy
+
+      print(sum)
+
+print('\nCourse approach\n')
+
+number=20730906
+tmpnumber = number
+sumOfDigits = 0
+
+while tmpnumber >0:
+    digit = tmpnumber % 10
+    sumOfDigits += digit
+    tmpnumber = tmpnumber//10
+else:
+    print('the sum of digits of ', number, ' is',sumOfDigits)
