60f9375b14e4ff217a64b1126ec89738eb7709ce
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-03-03 23:02:15 +0100

    session 1.2

--- 107_for_lab.py
+++ 107_for_lab.py
@@ -12,3 +12,35 @@ for i in range(0,fibonacci_iterations):
     b1 = b2
     b2 = b3
     b3 = b1+b2
+
+print('\nTask 2\n')
+
+text = "Industrial Light & Magic: In this case, you find Python used in the production process for scripting complex, computer graphic-intensive films. Originally, Industrial Light & Magic relied on Unix shell scripting, but it was found that this solution just couldn't do the job. Python was compared to other languages, such as Tcl and Perl, and chosen because it's an easier-to-learn language that the organization can implement incrementally. In addition, Python can be embedded within a larger software system as a scripting language, even if the system is written in a language such as C/C++. It turns out that Python can successfully interact with these other languages in situations in which some languages can't."
+
+words = text.split(' ')
+    
+for word in words:
+    if 'p' in word:
+        print(f'{word} has letter p')
+
+print('\nTask 3\n')
+
+dictionary={'A':'80%-100%','B':'60%-80%','C':'50-60%','D':'less than 50%'}
+
+for n in dictionary:
+    print(f'{n} - {dictionary[n]}')
+
+print('\nTask 3\n')
+
+text = "Industrial Light & Magic: In this case, you find Python used in the production process for scripting complex, computer graphic-intensive films. Originally, Industrial Light & Magic relied on Unix shell scripting, but it was found that this solution just couldn't do the job. Python was compared to other languages, such as Tcl and Perl, and chosen because it's an easier-to-learn language that the organization can implement incrementally. In addition, Python can be embedded within a larger software system as a scripting language, even if the system is written in a language such as C/C++. It turns out that Python can successfully interact with these other languages in situations in which some languages can't."
+
+words = text.split(' ')
+
+word_dictionary={}
+for word in words:
+    if word in word_dictionary.keys():
+        word_dictionary[word] = word_dictionary[word]+1
+    else:
+        word_dictionary.setdefault(word,1)
+        
+print(word_dictionary)
