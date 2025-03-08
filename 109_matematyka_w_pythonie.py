c34bc3ed407b01e7b4b442d5abcb5570251ce606
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-03-08 21:32:36 +0100

    Session 2.1

--- 109_matematyka_w_pythonie.py
+++ 109_matematyka_w_pythonie.py
@@ -1,6 +1,16 @@
-print('Lesson\n')
+print('---Lesson---\n')
 
 f_smaller = 3.141592653589793
 f_bigger = 3.87756392764
 
 print(type(f_smaller))
+print('\n')
+
+print(int(f_smaller), int(f_bigger)) # rzutowanie na int
+print('\n')
+
+print(abs(f_smaller), abs(-f_smaller)) # funkcja abs() zwraca wartość absolutną
+print('\n')
+
+print(round(f_smaller,2),round(f_bigger,2),round(f_bigger,3))
+print('\n')
