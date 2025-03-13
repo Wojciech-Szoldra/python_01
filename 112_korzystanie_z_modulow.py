fd46383e8ce50343f433966167815f941e304e35
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-03-13 21:28:18 +0100

    Session 1.1

---
+++ 112_korzystanie_z_modulow.py
@@ -0,0 +1,11 @@
+print('*** Lesson ***\n')
+
+import math # import modułu math
+
+pi = math.pi
+print(pi)
+
+# math floor zaokrągla do w dół do najbliższej liczby całkowitej
+result1 = math.floor(3.7)
+result2 = math.floor(math.pi)
+print(result1,result2)
