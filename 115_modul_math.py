b5aa625620a55b5d13eea46a912f9ff72e3550b0
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-03-16 13:35:48 +0100

    Session 1.1

---
+++ 115_modul_math.py
@@ -0,0 +1,33 @@
+print('*** Lesson ***\n')
+
+# import modułu
+import math
+
+wynik1 = math.ceil(4.2)
+print(f'wynik1: {wynik1}')
+
+wynik2 = math.ceil(-3.8) # w górę czyli bliżej zera
+print(f'wynik2: {wynik2}')
+
+wynik3 = math.floor(4.7)
+print(f'wynik3: {wynik3}')
+
+wynik4 = math.floor(-4.7) # w dół czyli odsuwamy się od 0
+print(f'wynik1: {wynik4}')
+
+wynik5 = math.pow(2,8) # math.pow() zwraca typ float
+print(f'wynik1: {wynik5}')
+
+wynik6 = math.pow(9,0.5) # podniesienie do potęgi 0.5 to pierwiastek kwadratowy
+print(f'wynik1: {wynik6}')
+
+wynik7 = math.sqrt(16)
+print(f'wynik1: {wynik7}')
+
+wynik8 = math.sin(math.pi/2)
+print(f'wynik1: {wynik8}')
+
+wynik9 = math.cos(math.pi/4)
+print(f'wynik1: {wynik9}')
+
+print('\n*** LAB ***')
