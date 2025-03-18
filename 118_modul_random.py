9cba37bf236838c57f297c6aa7bb3c370e544723
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-03-18 21:41:23 +0100

    Session 1.2

--- 118_modul_random.py
+++ 118_modul_random.py
@@ -18,3 +18,27 @@ print(random.choices(colors, weights=[10,1,1], k =2))
 numbers = [1,2,3,4,5]
 random.shuffle(numbers)
 print(numbers)
+
+print('\n*** LAB ***')
+print('\nTask 1\n')
+
+for i in range(1,11):
+    num1 = random.randint(1,100)
+    print(f'{i} random number: {num1}')
+
+print('\nTask 2\n')
+
+number1 = random.randint(1,100)
+number2 = 0
+counter = 1
+
+while number1 != number2:
+    number2 = random.randint(1,100)
+    print(f'Attempt {counter}: number1 - {number1} / number2 - {number2}')
+    
+    if number1 == number2:
+        print(f'It took {counter} tries')
+    
+    counter += 1
+
+    
