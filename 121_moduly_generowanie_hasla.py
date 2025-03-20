63c3d087792908568d7a9c52baf5d33bc46de02b
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-03-20 23:24:01 +0100

    Session 1.2

--- 121_moduly_generowanie_hasla.py
+++ 121_moduly_generowanie_hasla.py
@@ -4,12 +4,13 @@ import random
 for i in range(32,127):
     print(i, chr(i))
 
+# Funkcja ord() zmienia znak na jego wartość numeryczną Unicode
 test = ord('A')
 print(f'A in Unicode = {test}')
 
 print('\n')
 
-# Generowanie hasła
+# Generowanie hasła metoda 1
 ints = range(33, 127)
 password = ''
 
@@ -17,3 +18,23 @@ for i in range(16):
     password += chr(random.choice(ints))
 
 print(f'Password is: {password}')
+
+# Generowanie hasła metoda 2
+
+import secrets
+import string
+
+def generate_password(length=12):
+    alphabet = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(secrets.choice(alphabet) for i in range(length))
+    return password
+
+print(generate_password(16))
+
+# Funkcja os.urandom()
+import os
+
+random_bytes = os.urandom(10)
+print(random_bytes)
+random_numbers = list(random_bytes)
+print(random_numbers)
