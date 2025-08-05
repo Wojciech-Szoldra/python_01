918853503f2391798b1124cf8967473d6dee12dc
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-08-05 22:54:51 +0200

    Lab 05.08.2025

--- 134_modul_random_przyklad_lotto.py
+++ 134_modul_random_przyklad_lotto.py
@@ -19,3 +19,48 @@ while len(my_numbers) < 7:
     my_numbers.append(new_number)
 
 print(f'Those numbers will win: {my_numbers}')
+
+print('====LAB====\n')
+
+print('====ZAD 1====\n')
+
+import random
+
+colors = ['Hearts','Diamonds','Clubs','Spades']
+figures = ['Ace','King','Queen','Jack','10','9']
+
+all_cards = []
+
+for i in colors:
+    for j in figures:
+        all_cards.append(i+' '+j)
+
+print(f'Deck in order: {all_cards}')
+random.shuffle(all_cards)
+print(f'Shuffled deck: {all_cards}')
+
+print('='*40+'\n')
+
+player1 = []
+player2 = []
+
+max = len(all_cards)
+i=0
+
+allCards = []
+for c in colors:
+    for f in figures:
+        allCards.append("%s - %s" % (c, f))
+ 
+print(allCards)
+
+while i < (max-1):
+    if i % 2 == 0:
+        player1.append(all_cards[i])
+    else:
+        player2.append(all_cards[i])
+
+    i+=1
+
+print(f'Player 1: {player1}')
+print(f'Player 2: {player2}')
