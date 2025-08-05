ae9a379a289cb20c18fc5553db3db5403e36bbc9
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-08-05 20:20:12 +0200

    Lab 05.08.2025

--- 131_typy_timedelta_date_datetime.py
+++ 131_typy_timedelta_date_datetime.py
@@ -103,11 +103,61 @@ print('====ZAD 1====\n')
 import math
 
 inputdata = [0,1,2,3,5,8,13,21,34,55,89]
-factortable = [0,0.01,0.02, 0.03,0.05,0.08,0.13,0.21,0.34,0.55,0.89]
+factortable = [0,0.01,0.02,0.03,0.05,0.08,0.13,0.21,0.34,0.55,0.89]
+
 
 print(f'inputdata: {len(inputdata)} / factortable: {len(factortable)}')
 
+# Wersja 1
+
+
 if len(inputdata) == len(factortable):
-    print('OK')
+    n=0
+    for i in inputdata:
+        minvalue=inputdata[n]-factortable[n]*inputdata[n]
+        print(f'({n}) minvalue: {minvalue} / mininteger: {math.floor(minvalue)}')
+        maxvalue=inputdata[n]+factortable[n]*inputdata[n]
+        print(f'({n}) maxvalue: {maxvalue} / maxinteger: {math.ceil(maxvalue)}')
+        n+=1
 else:
     print('inputdata and factor table need to have equal number elements')
+
+# Wersja 2
+
+if len(inputdata) == len(factortable):
+    i=0
+    while i<len(inputdata):
+        minvalue=inputdata[i]-factortable[i]*inputdata[i]
+        maxvalue=inputdata[i]+factortable[i]*inputdata[i]
+        print('minvalue=',minvalue,'maxvalue=',maxvalue)
+        
+        mininteger = math.floor(minvalue)
+        maxinteger = math.ceil(maxvalue)
+        print(mininteger,"\t",inputdata[i],"\t",maxinteger)
+        i+=1
+else:
+    print("inputdata and factortable need to have equal number of elements")
+
+print('====ZAD 2====\n')
+
+import random
+
+n=0
+
+for i in inputdata:
+    minvalue=inputdata[n]-random.random()*inputdata[n]
+    maxvalue=inputdata[n]+random.random()*inputdata[n]
+    print(f'({n}) minvalue: {minvalue} / maxvalue: {maxvalue}')
+    
+    mininteger = math.floor(minvalue)
+    maxinteger = math.ceil(maxvalue)
+    print(mininteger,"\t",inputdata[n],"\t",maxinteger)
+    
+    n+=1
+else:
+    print('inputdata and factor table need to have equal number elements')
+
+print('====ZAD 3====\n')
+
+print(datetime.today())
+print(datetime.today().strftime("%Y-%m-%d"))
