a4ca9326dfab5ab087ede2de27074a71156331ad
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-08-12 20:57:58 +0200

    Lab: war card game

--- 137_138_petle_trojkat_pascala.py
+++ 137_138_petle_trojkat_pascala.py
@@ -55,5 +55,48 @@ for c in colors:
         a_card['Color'] = c
         all_cards.append(a_card)
 
-print(all_cards)
-print(len(all_cards))
+print(f'Deck in order: {all_cards}')
+random.shuffle(all_cards)
+print('\n')
+print(f'Shuffled deck: {all_cards}')
+
+print('\n')
+
+player1 = []
+player2 = []
+
+max = len(all_cards)
+
+for i in range(max):
+    if i % 2 == 0:
+        player1.append(all_cards[i])
+    else:
+        player2.append(all_cards[i])
+
+print(f'Player 1: {player1}\n')
+print(f'Player 2: {player2}')
+
+game=1
+
+while len(player1) > 0:
+    card1 = player1.pop(0)
+    card2 = player2.pop(0)
+
+    if card1['Power'] == card2['Power']:
+        player1.append(card1)
+        player2.append(card2)
+        print(f'Game:{game}    EQUAL  {card1['Power']} vs {card2['Power']}') 
+    elif card1['Power'] > card2['Power']:
+        player1.append(card1)
+        player1.append(card2)
+        print(f'Game:{game}    PLAY-1  {card1['Power']} vs {card2['Power']}  {'*'*len(player1)}') 
+    elif card1['Power'] < card2['Power']:
+        player2.append(card1)
+        player2.append(card2)
+        print(f'Game:{game}    PLAY-2  {card1['Power']} vs {card2['Power']}  {'*'*len(player2)}') 
+
+    game+=1
+
+print('Player 1 WON !!!') if len(player1) > 0 else print('Player 2 WON !!!')
+
+
