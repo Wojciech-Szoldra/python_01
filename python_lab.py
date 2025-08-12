4faff7df7a42199e4b772829cfde339ed75dcbb3
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-08-12 23:21:05 +0200

    tests

--- python_lab.py
+++ python_lab.py
@@ -164,3 +164,113 @@ print(ms % 10)
 ms2 = 'test2'
 ms2 += '%4d' % 10
 print(ms2)
+
+print('='*40)
+
+# Pop / Append
+
+num10 = [1,2,3,4,5]
+
+for i in range(2):
+    num_t = []
+    num_t = num10.pop(0)
+    print(num_t)
+
+num11 = [6,7,8]
+num10.extend(num11)
+print(num10)
+
+colors = ['Hearts','Diamonds','Clubs','Spades']
+figures = [
+    {'Figure':'Ace',  'Power':14},
+    {'Figure':'King', 'Power':13},
+    {'Figure':'Queen','Power':12},
+    {'Figure':'Jack', 'Power':11},
+    {'Figure':'10',   'Power':10},
+    {'Figure':'9',    'Power':9}]
+ 
+allCards = []
+for c in colors:
+    for f in figures:
+        aCard = f.copy()
+        aCard['Color'] = c
+        allCards.append(aCard)
+ 
+import random
+ 
+random.shuffle(allCards)
+print(allCards)
+ 
+player1 = []
+player2 = []
+ 
+player1 = allCards[:12]
+player2 = allCards[12:]
+ 
+ 
+print('-------PLAYER 1--------')
+print(player1)
+ 
+print('-------PLAYER 1--------')
+print(player2)    
+ 
+while len(player1) > 0 and len(player2) > 0:
+    card1 = player1.pop(0)
+    card2 = player2.pop(0)
+ 
+    stack = []  
+ 
+    if card1["Power"] == card2["Power"]:
+        while card1["Power"] == card2["Power"]:
+ 
+            print('WAR',card1['Power'],card2['Power'])
+            stack.append(card1)
+            stack.append(card2)
+ 
+            if len(player1)<2:
+                player2.extend(stack)    
+                player2.extend(player1)
+                player1 = []
+                print('PLAY-2 \t %d \t %d \t' % (card1["Power"], card2["Power"]) + len(player2)*'*')
+                break
+            elif len(player2)<2:
+                player1.extend(stack)    
+                player1.extend(player2)
+                player2 = []
+                print('PLAY-1 \t %d \t %d \t' % (card1["Power"], card2["Power"]) + len(player1)*'*')
+                break
+            else:
+                card1 = player1.pop(0)
+                card2 = player2.pop(0)
+                stack.append(card1)
+                stack.append(card2)
+                card1 = player1.pop(0)
+                card2 = player2.pop(0)
+        else:
+            if card1["Power"] > card2["Power"]:
+                stack.append(card1)
+                stack.append(card2)
+                player1.extend(stack)
+                print('PLAY-1 \t %d \t %d \t' % (card1["Power"], card2["Power"]) + len(player1)*'*')
+            else:
+                stack.append(card1)
+                stack.append(card2)
+                player2.extend(stack)
+                print('PLAY-2 \t %d \t %d \t' % (card1["Power"], card2["Power"]) + len(player2)*'*')
+            
+ 
+    elif card1["Power"] > card2["Power"]:
+        player1.append(card1)
+        player1.append(card2)
+        #print('PLAY-1 \t %d \t %d \t %d \t %d' % (card1["Power"], card2["Power"], len(player1), len(player2)))
+        print('PLAY-1 \t %d \t %d \t' % (card1["Power"], card2["Power"]) + len(player1)*'*')
+    else:
+        player2.append(card2)
+        player2.append(card1)
+        #print('PLAY-2 \t %d \t %d \t %d \t %d' % (card1["Power"], card2["Power"], len(player1), len(player2)))
+        print('PLAY-2 \t %d \t %d \t' % (card1["Power"], card2["Power"]) + len(player2)*'*')
+ 
+if len(player1) > 0:
+    print('PLAYER 1 WON!!!!')
+else:
+    print('PLAYER 2 WON!!!!')
