2e8be19d2fdb275bf5e2c0f7510defa8aa7440c4
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-08-20 19:28:36 +0200

    Lab

--- 143_funkcje_wprowadzenie.py
+++ 143_funkcje_wprowadzenie.py
@@ -5,3 +5,17 @@ def print_hello():
     return
 
 print_hello()
+
+print('====LAB====\n')
+
+print('====ZAD 1====\n')
+
+def print_cat():
+    txt = '''
+|\---/|
+| o_o |
+ \_^_/'''
+    print(txt)
+    return
+
+print_cat()
