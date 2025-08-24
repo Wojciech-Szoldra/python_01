cc9e19f0410b5fc796e16a5c43eefbcb00add018
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-08-24 14:40:02 +0200

    Lab

--- 155_zwracanie_wartosci_w_funkcji.py
+++ 155_zwracanie_wartosci_w_funkcji.py
@@ -21,4 +21,65 @@ def give_working_day(year=date.today().year,
 next_work = give_working_day()
 print(next_work)
 
+#Wyświetlenie zwróconej wartości bezpośrednio w print()
 print(give_working_day())
+
+print('====LAB====\n')
+
+print('====ZAD 1====\n')
+
+def print_animal(animal=''):
+
+    txt_cat ='''
+    |\---/|
+    | o_o |
+     \_^_/
+    '''
+    txt_bear ='''
+    /  \.-"""-./  \\
+    \    -   -    /
+    |   o   o   |
+    \  .-'"'-.  /
+     '-\__Y__/-'
+        `---`
+    '''
+    txt_bat ='''
+      /\                 /\\
+     /  '._   (\_/)   _.'  \\
+    /_.''._'--('.')--'_.''._\\
+    | \_ / `;=/ " \=;` \ _/ |
+     \/ `\__|`\___/`|__/` \/
+             \(/|\)/  
+    '''
+    
+    if animal == 'cat':
+        print(txt_cat)
+        return True
+    elif animal == 'bear':
+        print(txt_bear)
+        return True
+    elif animal == 'bat':
+        print(txt_bat)
+        return True
+    else:
+        print(f'Cannot print \'{animal}\'\nChoose correct value: cat, bear, bar')
+        return False
+    
+variable_t_f = print_animal('')
+print(variable_t_f)
+
+print('====ZAD 2====\n')
+
+from datetime import date
+
+def how_many_days_left(year=date.today().year,
+                       month=date.today().month,
+                       day=date.today().day):
+    
+    today = date(year,month,day)
+    current_year = today.year
+    last_day = date(current_year,12,31)
+    days_number = last_day - today
+    return days_number.days
+
+print(how_many_days_left())
