478e2f2c302c2bded35a359e54f2646305b1bd82
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-10-01 22:32:54 +0200

    Lab

--- 179_obsluga_bledu_try.py
+++ 179_obsluga_bledu_try.py
@@ -10,6 +10,33 @@ try:
     task_per_person = tasks / people
 
 except:
-    print(f'Something went wrong… {sys.exc_info()}')
+    print(f'Something went wrong… {sys.exc_info() [0]}')
 
 print(f'Every person should have around {task_per_person} tasks')
+
+print('====LAB====\n')
+
+print('====ZAD 1====\n')
+
+import sys
+
+file_path = r'D:\IT\Programowanie\testy\txt\orders.csv'
+
+try: 
+    with open(file_path,"r") as file:
+    
+        for line in file:
+    
+            line = line.replace('\n','')
+            order = line.split(',')
+    
+            pharmacy_name = order[0]
+            item = order[1]
+            amount = int(order[2])
+
+            print(f'Order form drugstore {pharmacy_name}, item: {item}, amount: {amount}')
+except:
+    print(f'Problem with line: {line} \n {sys.exc_info() [0]}')
+
+ 
+print("Processing finished")
