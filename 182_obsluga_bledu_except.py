0e999d9e09224afcff80ee4c2fa94b0b1961e573
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-10-02 20:43:47 +0200

    Lab

--- 182_obsluga_bledu_except.py
+++ 182_obsluga_bledu_except.py
@@ -21,3 +21,37 @@ except:
     print(f'Something went wrong: {sys.exc_info() [0]}')
 
 print(f'Every person should have around {task_per_person} tasks')
+
+print('====LAB====\n')
+
+print('====ZAD 1====\n')
+
+import sys
+
+file_path = r'D:\IT\Programowanie\testy\txt\orders.csv'
+
+try: 
+    with open(file_path,"r") as file:
+    
+        for line in file:
+    
+            line = line.replace('\n','')
+            order = line.split(',')
+    
+            pharmacy_name = order[0]
+            item = order[1]
+            amount = int(order[2])
+
+            print(f'Order form drugstore {pharmacy_name}, item: {item}, amount: {amount}')
+
+except ValueError as e:
+    print(f'Problem with converting data to integer in line: {line}')
+
+except IndexError as e:
+    print(f'To many data in line: {line}')
+
+except:
+    print(f'Problem with line: {line} \n {sys.exc_info() [0]}')
+
+ 
+print("Processing finished")
