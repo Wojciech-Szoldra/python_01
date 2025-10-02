a14960b3ba46c5febc3c08d9def32633f54ad979
Wojciech Szołdra <wojciech.szoldra@gmail.com>
2025-10-02 20:10:25 +0200

    End of the lesson

---
+++ 182_obsluga_bledu_except.py
@@ -0,0 +1,23 @@
+# try...except -> obsługa błędów w zależności od typu
+# as e -> dodatkowe informacje o błędzie, które można wykorzystać 
+import sys
+
+task_per_person = 0
+
+try:
+    tasks = 32
+    people_str = input('How many people are in the team? ')
+    people = int(people_str)
+
+    task_per_person = tasks / people
+
+except ValueError as e:
+    print(f'Sorry - you need to enter an integer number / {e}')
+
+except ZeroDivisionError as e:
+    print(f'Sorry - you need to enter value > 0 / {e}')
+
+except:
+    print(f'Something went wrong: {sys.exc_info() [0]}')
+
+print(f'Every person should have around {task_per_person} tasks')
